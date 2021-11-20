f89c375d3d17a1a3919f79507d94ea8025336a34
apple <apple@apples-MacBook.local>
2021-11-20 20:19:26 +0800

    heroku

--- start.sh
+++ start.sh
@@ -1,3 +1,23 @@
 #!/usr/bin/env sh
 
+# heroku
+if [ $PORT ];then
+cat << EOF > /etc/nginx/sites-enabled/wstunnel
+server {
+	listen $PORT;
+	listen [::]:$PORT;
+
+	location /wstunnel {
+        proxy_pass http://127.0.0.1:10033;
+        proxy_http_version  1.1;
+
+        proxy_set_header    Upgrade \$http_upgrade;
+        proxy_set_header    Connection \"upgrade\";
+        proxy_set_header    Host \$http_host;
+        proxy_set_header    X-Real-IP \$remote_addr;
+    }
+}
+EOF
+fi
+
 supervisord -n
